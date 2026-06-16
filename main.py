c61507d2b5d8302d990da64eae258fad34541cc8
David <davud.gzade@gmail.com>
2026-06-16 15:23:23 +0400

    added loop epta

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import os
 import time
 
@@ -7,7 +9,7 @@ def python():
     lines = []
     while True:
         line = input()
-        if not line: break #полный пиздец бляяяяяя
+        if not line: break 
         lines.append(line)
     with open(filename, "w") as f:
         f.write("\n".join(lines))
@@ -51,42 +53,49 @@ def choose():
 
 def begin():
     print("λ Ide//")
-    asd = input("")
     config = "lambda.txt"
-    
-    if asd == "choose lang":
-        choose()
-    elif asd == "inst_comp - propertiesetc":
-        print("If no compilers installed choose a compiler you want to install if all is installed type N:")
-        print("Python, C++, C,")
-        if asd.lower() == "python":
-            os.system('brew install python3')
-            time.sleep(7)
-            os.system('y')
-            time.sleep(45)
-            print("is installed!")
-        elif asd.lower() == "c" or asd.lower() == "c++":
-            os.system('brew install clang')
-            time.sleep(15)
-            print("is installed!")
-        else:
-            print("error via installing")
-    elif asd == "y":
-        os.chdir('configs&brainfuckingstuff')
-        with open(config, "w") as f:
-            if "language add: java" in config:
-                os.system('brew install java')
-    elif asd == "n" or asd == "N":
-        choose()
-    elif asd.lower() == "show_config":
-        with open(config, 'r', encoding='utf-8') as file:
-            content = file.read()
-            print(content)
-    elif asd.lower() == "edit config":
-        edit = input("")
-        with open(config, "a") as file:
-            file.write(edit)
-    else:
-        print("error via editing config")
+    while True:
+        asd = input("")
 
+        if asd == "choose lang":
+            choose()
+        elif asd == "inst_comp - propertiesetc":
+            print("If no compilers installed choose a compiler you want to install if all is installed type N:")
+            print("Python, C++, C,")
+            if asd.lower() == "python":
+                os.system('brew install python3')
+                time.sleep(7)
+                os.system('y')
+                time.sleep(45)
+                print("is installed!")
+            elif asd.lower() == "c" or asd.lower() == "c++":
+                os.system('brew install clang')
+                time.sleep(15)
+                print("is installed!")
+            else:
+                print("error via installing")
+        elif asd == "y":
+            os.chdir('configs&brainfuckingstuff')
+            with open(config, "w") as f:
+                if "language add: java" in config:
+                    os.system('brew install java')
+        elif asd == "n" or asd == "N":
+            choose()
+        elif asd.lower() == "show_config":
+            with open(config, 'r', encoding='utf-8') as file:
+                content = file.read()
+                print(content)
+        elif asd.lower() == "edit config":
+            edit = input("")
+            with open(config, "a") as file:
+                file.write(edit)
+            time.sleep(2)
+            print("File edited")
+        elif asd.lower() == "exit":
+            break
+        else:
+            print("error via editing config")
+    print("Exiting IDE.")
+    
 begin()
+  
